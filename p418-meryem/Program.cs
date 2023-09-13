df24cebc56dcaf1becdbaa0e96bd414b02bd1c7c
mariama1ieva <maryamfa@code.edu.az>
2023-09-13 18:59:26 +0400

    added csharp tasks

---
+++ p418-meryem/Program.cs
@@ -0,0 +1,189 @@
+﻿//task1
+//int m = 10;
+//for (int i = 0; i < m; i++) 
+
+//{
+//    Console.WriteLine(i);
+//}
+
+//task2 
+
+//int m = 6;
+//int count = 1;
+//for (int i  = 0; i < m; i++)
+//{
+//    Console.WriteLine(count++);
+//}
+
+
+
+
+//using System.ComponentModel.Design;
+//using System.Diagnostics.CodeAnalysis;
+//using System.Net.WebSockets;
+//using System.Reflection;
+//task3
+
+//int m = 4;
+//int sum = 0;
+//for (int i = 0; i < m; i++)
+//{
+//    sum += i;
+
+//} 
+//    Console.WriteLine(sum);
+
+//task4
+
+//    int m =5;
+//int count = 0;
+//for (int i = 0; i < m; i++)
+//{
+//    if (i % 2 == 1)
+//    {
+//        count++;
+//    }
+
+//}
+//Console.WriteLine(count);
+
+//task5
+//int m = 10;
+//int n = 4;
+//int sum = 0;
+// for (int i = n; i <= m; i++)
+//{
+//    if (i % 2 == 0)
+//    {
+//        sum += i;
+//    }
+//}
+//Console.WriteLine(sum);
+
+
+//task6
+
+//int m = 2;
+//int n = 5;
+//int sum = 0;
+//for (int i =  m; i <=n; i++)
+//{
+//    if (i % 2 == 0)
+//    {
+//        sum += i;
+//    }
+//}
+//Console.WriteLine(sum);
+
+//int fakt = 1;
+//for(int j = sum; j >0; j--)
+//{
+//    fakt = fakt * j;
+//}
+//Console.WriteLine(fakt);
+
+
+//task7
+//int[] nums = { 2, 3, 4, 1, 5, 7, 3, 6 };
+//int multiple = 1;
+//for (int i = 0; i < nums.Length; i++)
+//{
+//    if (nums[i] % 2 == 0)
+//    {
+//        multiple *= nums[i];
+//    }
+
+//}
+//Console.WriteLine(multiple);
+
+////task8
+//int n = 17;
+
+//if (n % 3 == 0 && n % 7 == 0)
+//{
+//    Console.WriteLine("bolunur");
+//}
+//else 
+//{
+//    Console.WriteLine("bolunmur");
+//}
+
+//task9
+//int m = 6;
+//int n = 3;
+//int sum = 0;
+//if (m % 2 == 0 && n % 2 == 0)
+//{
+//    sum = n + m;
+//    Console.WriteLine(sum);
+//}
+//else
+//{
+//    Console.WriteLine("cut deyil");
+//}
+
+////task10 
+//int n = 4;
+//int m = 8;
+//int sum = 0;
+//for (int i = n; i < m; i++)
+//{
+//    if (i%2==1)
+//    {
+//        sum += i;
+//    }
+//}
+//Console.WriteLine(sum);
+
+//task11
+//int[] numbers = { 2, 6, 3, 7, 1, 9, 1 };
+//int sum = 0;
+//for (int i = 0; i < numbers.Length; i++)
+
+//{
+//    if (numbers[i] % 2 == 1)
+//    {
+//        sum+= numbers[i];  
+//    }
+//}
+//Console.WriteLine(sum);
+
+////task12
+//int[] numbers = { 2, 6, 3, 7, 1, 9, 4 };
+//int count = 0;
+//for (int i = 0; i < numbers.Length; i++)
+//{
+//    if (numbers[i]%2==0)
+//    {
+//        count++;    
+//    }
+//}
+//Console.WriteLine(count);
+
+////task13
+//using System.ComponentModel;
+
+//using System.ComponentModel.Design;
+
+////Task 14
+//using System.ComponentModel.Design;
+
+//int n = 5;
+//int count = 0;
+//for (int i = 1; i <= n; i++) 
+//{
+//    if (n % i == 0)
+//    {
+//        count++;
+//    }   
+//}
+
+//    if(count == 2)
+//{
+//    Console.WriteLine("eded sadedir");
+
+//}
+//else
+//{
+//    Console.WriteLine("eded murekkebdir");
+}
